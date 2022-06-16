b62f53d5ba37ab3e5112290856c7270f7b2ea06c
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-16 08:51:21 +0300

    commit tree df69e864d6aab7499261449aedb6743c6c42765e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name : `${userName}`,
+            email: `${userEmail}`,
+
+        })
+    }
+
+
+
+    return fetch('http://localhost:3000/users',configurationObject)
+    .then(response => response.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    } )
+    .catch((error) => {
+        alert('Oops!')
+        document.body.innerHTML = error.message;
+    })
+
+}
